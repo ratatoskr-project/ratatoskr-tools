207dc1d296de2caa05cb038769154b95c7e89f27
yangyt96 <yee.yang.tan@rwth-aachen.de>
2021-02-22 20:15:20 +0100

    [docs] add description for datahandle functions

--- ratatoskr_tools/datahandle/__init__.py
+++ ratatoskr_tools/datahandle/__init__.py
@@ -1,57 +1,98 @@
-from .combine_hists import *
 import pandas as pd
+
+from .combine_hists import *
 
 
 def retrieve_vc_usages(simdirs):
+    """
+    Retrieve all the vc usages simulation result from the dummy simulation directories.
 
-    VCUsage_inj = [pd.DataFrame() for i in range(3)]
+    Parameters
+    ----------
+    simdirs : list(str)
+        The list of dummy simulation directories.
+
+    Returns
+    -------
+    list(pd.DataFrame)
+        The retrieved vc usages.
+    """
+
+    vc_usage_inj = [pd.DataFrame() for i in range(3)]
 
     for simdir in simdirs:
-        VCUsage_run = combine_vc_hists(simdir + "/VCUsage")
+        vc_usage_run = combine_vc_hists(simdir + "/VCUsage")
 
-        if VCUsage_run is not None:
-            for ix, layer_df in enumerate(VCUsage_run):
-                VCUsage_inj[ix] = pd.concat([VCUsage_inj[ix], layer_df])
+        if vc_usage_run is not None:
+            for ix, layer_df in enumerate(vc_usage_run):
+                vc_usage_inj[ix] = pd.concat([vc_usage_inj[ix], layer_df])
 
     # Calculate the average and std for VC usage.
-    VCUsage_temp = []
-    for df in VCUsage_inj:
+    vc_usage_temp = []
+    for df in vc_usage_inj:
         if not df.empty:
-            VCUsage_temp.append(df.groupby(df.index).agg(['mean', 'std']))
+            vc_usage_temp.append(df.groupby(df.index).agg(['mean', 'std']))
 
-    return VCUsage_temp
+    return vc_usage_temp
 
 
 def retrieve_buff_usages(simdirs):
+    """
+    Retrieve all the buff usages simulation result from the dummy simulation directories.
 
-    BuffUsage_inj = init_data_structure()
+    Parameters
+    ----------
+    simdirs : list(str)
+        The list of dummy simulation directories.
+
+    Returns
+    -------
+    dict
+        The retrieved buff usages.
+    """
+
+    buff_usage_inj = init_data_structure()
 
     for simdir in simdirs:
-        BuffUsage_run = combine_buff_hists(simdir + "/BuffUsage")
-        if BuffUsage_run is not None:
-            for l in BuffUsage_inj:
-                for d in BuffUsage_inj[l]:
-                    BuffUsage_inj[l][d] = BuffUsage_inj[l][d].add(
-                        BuffUsage_run[l][d], fill_value=0)
+        buff_usage_run = combine_buff_hists(simdir + "/BuffUsage")
+        if buff_usage_run is not None:
+            for l in buff_usage_inj:
+                for d in buff_usage_inj[l]:
+                    buff_usage_inj[l][d] = buff_usage_inj[l][d].add(
+                        buff_usage_run[l][d], fill_value=0)
 
     # Average the buffer usage over restarts.
-    BuffUsage_temp = init_data_structure()  # a dict of dicts
-    for l in BuffUsage_inj:
-        for d in BuffUsage_inj[l]:
-            BuffUsage_temp[l][d] = np.ceil(BuffUsage_inj[l][d] / len(simdirs))
+    buff_usage_temp = init_data_structure()  # a dict of dicts
+    for l in buff_usage_inj:
+        for d in buff_usage_inj[l]:
+            buff_usage_temp[l][d] = np.ceil(
+                buff_usage_inj[l][d] / len(simdirs))
 
-    return BuffUsage_temp
+    return buff_usage_temp
 
 
 def retrieve_diff_latencies(simdirs):
+    """
+    Retrieve all kinds of latencies (flit, packet, network) simulation result from the dummy simulation directories.
 
-    latencyFlits = -np.ones(len(simdirs))
-    latencyPackets = -np.ones(len(simdirs))
-    latencyNetworks = -np.ones(len(simdirs))
+    Parameters
+    ----------
+    simdirs : list(str)
+        The list of dummy simulation directories.
+
+    Returns
+    -------
+    tuple
+        The retrieved latencies in the order of flit, packet, network.
+    """
+
+    latency_flits = -np.ones(len(simdirs))
+    latency_packets = -np.ones(len(simdirs))
+    latency_networks = -np.ones(len(simdirs))
     for idx, simdir in enumerate(simdirs):
         lat = get_latencies(simdir + "/report_Performance.csv")
-        latencyFlits[idx] = lat[0]
-        latencyPackets[idx] = lat[1]
-        latencyNetworks[idx] = lat[2]
+        latency_flits[idx] = lat[0]
+        latency_packets[idx] = lat[1]
+        latency_networks[idx] = lat[2]
 
-    return latencyFlits, latencyPackets, latencyNetworks
+    return latency_flits, latency_packets, latency_networks
