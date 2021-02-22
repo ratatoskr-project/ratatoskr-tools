c4c876144dcea7ac4673976670461d059d645049
yangyt96 <yee.yang.tan@rwth-aachen.de>
2021-02-22 11:52:41 +0100

    [init] add the setup of the package

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name='ratatoskr-tools',
+    version='0.1',
+    author='Jan Moritz Joseph',
+    author_email='',
+    description='API to automate ratatoskr simulation.',
+    url='',
+    license='MIT',
+    packages=find_packages(exclude=['docs*', 'tests*']),
+    data_files={
+        'config': ['./networkconfig/config.ini']
+    },
+    entry_points={
+        'console_scripts': ['']
+    },
+
+)
