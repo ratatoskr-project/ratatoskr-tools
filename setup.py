6702cd9b5ad72db0295bb7527a578b71b74c63de
yangyt96 <yee.yang.tan@rwth-aachen.de>
2021-02-22 19:09:17 +0100

    [chore] add non *.py file during installation and edit info
    	- make ratatoskr_tools/networkconfig/config.ini is added during the package installation

--- setup.py
+++ setup.py
@@ -3,14 +3,17 @@ from setuptools import find_packages
 
 setup(
     name='ratatoskr-tools',
-    version='0.1',
-    author='Jan Moritz Joseph',
-    author_email='',
+    version='2021.02',
+    author='Jan Moritz Joseph, Yee Yang Tan',
+    author_email='joseph@ice.rwth-aachen.de',
     description='API to automate ratatoskr simulation.',
-    url='',
+    url='https://github.com/jmjos/ratatoskr-tools',
     license='MIT',
     packages=find_packages(exclude=['docs*', 'tests*']),
-
+    package_data={
+        '': ['ratatoskr_tools/networkconfig/config.ini']
+    },
+    include_package_data=True,
     entry_points={
         'console_scripts': ['']
     },
